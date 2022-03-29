884c160ec5c41d149b93f4eb8613323d92e64ea6
Abishek <abishekhari09@gmail.com>
2022-03-29 15:36:01 +0530

    message

---
+++ script.js
@@ -0,0 +1,26 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+var swiper = new Swiper(".image-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableonInteraction: false,
+  },
+});
